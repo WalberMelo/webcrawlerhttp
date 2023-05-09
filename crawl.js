3e0812ed1a62f267bf172a65f00e6a4f1150d87b
Walber Melo <dev4wm@gmail.com>
2023-05-09 15:13:47 +0100

    crwal page recursive working

--- crawl.js
+++ crawl.js
@@ -1,29 +1,54 @@
 const { JSDOM } = require("jsdom");
+const axios = require("axios");
 
-async function crawlPage(currentURL) {
-  console.log(`actively crawling: ${currentURL}`);
-
+async function crawlPage(baseURL, currentURL, pages) {
   try {
-    const res = await fetch(currentURL);
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+      return pages;
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if (pages[normalizedCurrentURL] > 0) {
+      pages[normalizedCurrentURL]++;
+      return pages;
+    }
+
+    pages[normalizedCurrentURL] = 1;
+
+    console.log(`actively crawling: ${currentURL}`);
+
+    const res = await axios.get(currentURL);
     if (res.status > 399) {
       console.log(
         `error in fetch with status code: ${res.status} on page: ${currentURL}`
       );
+      return pages;
     }
 
-    const contentType = res.headers.get("content-type");
+    const contentType = res.headers["content-type"];
     if (!contentType.includes("text/html")) {
       console.log(
         `No html response, content type: ${contentType}, on page: ${currentURL}`
       );
+      return pages;
     }
 
-    const data = await res.text();
-    console.log(data);
+    const htmlBody = res.data;
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (error) {
     console.log(`error in fetch: ${error.message}, on page: ${currentURL}`);
   }
+  return pages;
 }
+
+// Rest of the code remains the same
 
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { crawlPage } = require("./crawl");
+const { crawlPage } = require("./crawl.js");
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("no website provided");
     process.exit(1);
@@ -10,13 +10,16 @@ function main() {
     console.log("To many command line args");
     process.exit(1);
   }
-  for (const arg of process.argv) {
-    console.log(arg);
-  }
 
   const baseURL = process.argv[2];
   crawlPage(baseURL);
+
   console.log(`Starting crawl of ${baseURL}`);
+  const pages = await crawlPage(baseURL, baseURL, {});
+
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 }
 
 main();
