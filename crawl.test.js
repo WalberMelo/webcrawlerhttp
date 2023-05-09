e17579c93f8ad5f05447f596561ff28a51c68f8d
Walber Melo <dev4wm@gmail.com>
2023-05-09 13:48:19 +0100

    getURLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL stripe protocol", () => {
@@ -29,6 +29,76 @@ test("normalizeURL stripe http", () => {
   const input = "http://blog.boot.dev/path/";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+<html>
+<body>
+  <header>
+    <h1>Welcome to my Website</h1>
+    <nav>
+      <ul>
+        <li><a href="https://blog.boot.dev/path/">Boot.dev Blog</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
+ `;
+  const inputBaseURL = "https://blog.boot.dev/path/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/path/"];
+
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+<html>
+<body>
+  <header>
+    <h1>Welcome to my Website</h1>
+    <nav>
+      <ul>
+        <li><a href="/home/">Home</a></li>
+        <li><a href="/about/">About us</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
+ `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://blog.boot.dev/home/",
+    "https://blog.boot.dev/about/",
+  ];
+
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTMLBody = `
+<html>
+<body>
+  <header>
+    <h1>Welcome to my Website</h1>
+    <nav>
+      <ul>
+        <li><a href="invalid">Home</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
+ `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
 
   expect(actual).toEqual(expected);
 });
